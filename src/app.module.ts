68bc287abd2aeb4513a27e2192d0b68954b91de9
mozar86 <mozarfrancisco@gmail.com>
2025-01-31 20:18:24 -0300

    Correção de SSL para PostgreSQL no Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,10 +18,12 @@ import { ProductsModule } from './products/products.module';
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        ssl: {
-          rejectUnauthorized: false, 
+        synchronize: true, 
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
-        // synchronize: true, 
       }),
     }),
     AuthModule,
@@ -29,42 +31,3 @@ import { ProductsModule } from './products/products.module';
   ],
 })
 export class AppModule {}
-
-
-/*
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { ProductsModule } from './products/products.module';
-import { DataSource } from 'typeorm';
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'), 
-        autoLoadEntities: true,
-        //synchronize: true, 
-      }),
-    }),
-    AuthModule,
-    ProductsModule,
-  ],
-})
-export class AppModule {}
-
-JWT_SECRET=ml163086
-JWT_EXPIRES_IN=3600
-PORT=3000
-
-DB_HOST=dpg-cudodblumphs73cqttq0-a
-DB_PORT=5432
-DB_USER=admin
-DB_PASSWORD=DYVByz5oqVveE5qcFco5LY70doOGbDiN
-DB_NAME=culture_code_hh3f
-*/
